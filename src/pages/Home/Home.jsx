88cb93e990d23d1d0d60c51b3ad6c8fbabdbb7e5
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-12-10 19:51:09 +0600

    add home page layout and date time formatting

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import Header from "../shared/Header/Header";
+import LeftSideNev from "../shared/LeftSideNav/LeftSideNev";
 import Navber from "../shared/Navbar/Navber";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navber></Navber>
-            <h2 className="text-4xl font-poppins font-bold">This is home</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNev></LeftSideNev>
+                </div>
+                <div className="md:col-span-2">
+                    <h2 className="text-2xl">Dragon News Home</h2>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
