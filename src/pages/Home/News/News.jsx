b889748a497ecb6de1ac2426b5060594b7d294fe
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-12-11 21:33:09 +0600

    add privetRoute and do not redirect on reload

--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -1,8 +1,23 @@
+import Header from "../../shared/Header/Header";
+import RightSideNav from "../../shared/RightSideNav/RightSideNav";
+import Navber from "../../shared/Navbar/Navber";
+import { useParams } from "react-router-dom";
 
 const News = () => {
+    const {id} = useParams()
+    console.log(id)
     return (
         <div>
-            <h1>News Details</h1>
+            <Header></Header>
+            <Navber></Navber>
+            <div className="grid grid-cols-4">
+                <div className="col-span-3" >
+                <h1 className="text-[20px] text-[#403F3F] font-semibold" >Dragon News</h1>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
